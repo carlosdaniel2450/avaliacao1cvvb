ef36a3736fbeda8770e1e004d98c42f6468e1c97
CArlosRyokko <143403170+CArlosRyokko@users.noreply.github.com>
2023-09-25 08:32:53 -0300

    avaliação
    pedro paulo e carlos daniel

---
+++ 001.py
@@ -0,0 +1,7 @@
+n1 = int( input("digite numero:"))
+n2 = int( input("digite numero:"))
+n3 = int( input("digite numero:"))
+n4 = int( input("digite numero:"))
+n5 = int( input("digite numero:"))
+r = n1 + n2 + n3 + n4 + n5
+print("resultado:",r)
